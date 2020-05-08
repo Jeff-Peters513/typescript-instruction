4f3b6d9b43daa29244b7cddddaa29075f4895c00
Jeff Peters <petersjeff288@gmail.com>
2020-05-07 23:15:12 -0400

    modified bowling game

--- bowling/ts/bowling.ts
+++ bowling/ts/bowling.ts
@@ -1,31 +1,31 @@
-let min: number;
-let max: number; 
+//let min: any;
+//let max: any;
+let score:number = 0;
+let tScore: number = 0;
 
 let readline = require('readline-sync');
 
 console.log("Welcome to Virtual Bowling!")
 console.log("----------------------------");
-
 let command: string = "";
-command = readline.question("Command?  ");
+command = readline.question("Wanna Play? (y/n):  "); 
 
-
-while(command != "y") {
-    console.log("Command Menu");
-    console.log("-------------");
-    console.log("list --list all movies");
-    console.log("get --get single movie");
-    console.log("add --a movie");
-    console.log("del --delete movie");
-    console.log("exit --exit the app\t");
-
-    
-
-
-
-getRandomNumberBetween(1,10);
-
-
+while(command != "n") {
+   for (let idx = 1; idx < 10; idx++) {
+        console.log("-----------------------");
+        score = getRandomNumberBetween("1", "10")
+        console.log("Bowl "+idx+ " scored  "+score);
+        tScore += score;
+        console.log("-----------------------");
+   }
+    console.log("***********************");
+    console.log("Total score: "+tScore);  
+    console.log("");
+    tScore = 0;
+    command = readline.question("Wanna Play? (y/n):  ");
+}
+console.log("");
+console.log("Bye!");
 
 function getRandomNumberBetween(min,max){
     return Math.floor(Math.random()*(max-min+1)+min);
